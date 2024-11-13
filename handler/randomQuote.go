5b6a2a4746a41f282a2847cc6475f5224eb29981
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-11-13 22:26:33 +0000

    changed author info

--- handler/randomQuote.go
+++ handler/randomQuote.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"log"
 	"math/rand/v2"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,9 +12,12 @@ import (
 )
 
 type Response struct {
-	Quote  string `json:"quote"`
-	Author string `json:"author"`
-	Book   string `json:"book"`
+	Quote   string `json:"quote"`
+	Author  string `json:"author"`
+	Book    string `json:"book"`
+	Bio     string `json:"bio"`
+	ImgLink string `json:"imglink"`
+	Books   []Book `json:"books"`
 }
 
 func GetRandomQuote(c *fiber.Ctx, db *database.Queries, cacheQuote *cache.CacheQuote) error {
@@ -29,19 +34,48 @@ func GetRandomQuote(c *fiber.Ctx, db *database.Queries, cacheQuote *cache.CacheQ
 		cacheQuote.Quote.Book = quote.Book
 		cacheQuote.Quote.Quote = quote.Quote
 		cacheQuote.Expiration = time.Now().Add(24 * time.Hour)
+		authorInf := getAuthorinfo(quote.Author)
 		resp = Response{
-			Quote:  quote.Quote,
-			Author: quote.Author,
-			Book:   quote.Book,
+			Quote:   quote.Quote,
+			Author:  quote.Author,
+			Book:    quote.Book,
+			Bio:     authorInf.Bio,
+			ImgLink: authorInf.ImgLink,
+			Books:   authorInf.Books,
 		}
 
 		return c.JSON(resp)
 	}
+	authorInf := getAuthorinfo(cacheQuote.Quote.Author)
+
 	resp = Response{
-		Quote:  cacheQuote.Quote.Quote,
-		Author: cacheQuote.Quote.Author,
-		Book:   cacheQuote.Quote.Book,
+		Quote:   cacheQuote.Quote.Quote,
+		Author:  cacheQuote.Quote.Author,
+		Book:    cacheQuote.Quote.Book,
+		Bio:     authorInf.Bio,
+		ImgLink: authorInf.ImgLink,
+		Books:   authorInf.Books,
 	}
 
 	return c.JSON(resp)
+}
+
+func getAuthorinfo(authorName string) AuthorInfo {
+	AuthorResp := AuthorInfo{}
+	authors, err := database.LoadAuthors("database.json")
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, author := range authors.Authors {
+		if strings.Contains(authorName, author.Authorname) {
+			AuthorResp.AuthorName = author.Authorname
+			AuthorResp.Bio = author.Description
+			AuthorResp.ImgLink = author.ImgLink
+			for _, book := range author.Books {
+				AuthorResp.Books = append(AuthorResp.Books, book)
+			}
+		}
+	}
+	return AuthorResp
 }
